227b6252ae2067b77967fa56b56e7f8a0d218437
fazy777 <hafizmfaizanali26688@gmail.com>
2026-07-20 17:53:44 +0500

    Add Frontend and Backend Java contact management system

---
+++ Backend/contact_managment_system/main_application/src/main/java/com/contact_managment/main_application/MainApplication.java
@@ -0,0 +1,13 @@
+package com.contact_managment.main_application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainApplication.class, args);
+	}
+
+}
---
+++ Backend/contact_managment_system/main_application/src/test/java/com/contact_managment/main_application/MainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.contact_managment.main_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
